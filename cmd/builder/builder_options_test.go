b024a6d57a48eab0cfce67a4ac5861eb7e9c1064
Yuri Shkuro <ys@uber.com>
2018-01-01 16:30:45 -0500

    Fix tests
    Signed-off-by: Yuri Shkuro <ys@uber.com>

--- cmd/builder/builder_options_test.go
+++ cmd/builder/builder_options_test.go
@@ -20,24 +20,14 @@ import (
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 
-	cascfg "github.com/jaegertracing/jaeger/pkg/cassandra/config"
-	escfg "github.com/jaegertracing/jaeger/pkg/es/config"
-	"github.com/jaegertracing/jaeger/storage/spanstore/memory"
 	"github.com/uber/jaeger-lib/metrics"
 )
 
 func TestApplyOptions(t *testing.T) {
 	opts := ApplyOptions(
-		Options.CassandraSessionOption(&cascfg.Configuration{}),
 		Options.LoggerOption(zap.NewNop()),
 		Options.MetricsFactoryOption(metrics.NullFactory),
-		Options.MemoryStoreOption(memory.NewStore()),
-		Options.ElasticClientOption(&escfg.Configuration{
-			Servers: []string{"127.0.0.1"},
-		}),
 	)
-	assert.NotNil(t, opts.CassandraSessionBuilder)
-	assert.NotNil(t, opts.ElasticClientBuilder)
 	assert.NotNil(t, opts.Logger)
 	assert.NotNil(t, opts.MetricsFactory)
 }
